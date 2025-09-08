aa37b2f2edf7e7fe96c68e26054644fe6b49d310
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-09-08 09:36:01 +0300

    feature: class Circle

---
+++ shapes/circle.py
@@ -0,0 +1,10 @@
+from .shape import Shape
+from math import pi
+
+
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def get_square(self):
+        return pi * self.radius ** 2
